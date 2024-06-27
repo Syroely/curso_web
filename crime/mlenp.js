dd92303a3523e96638a41c7082eb6bfc59d11181
Syroely <158362496+Syroely@users.noreply.github.com>
2024-06-27 19:54:52 -0300

    Update mlenp.js

--- crime/mlenp.js
+++ crime/mlenp.js
@@ -1,3 +1,7 @@
-function teste() {
-    document.getElementById('btn_a1').value = "C"
+function tundra() {
+    var botao = document.getElementById('btn_a1');
+    vet = ["falsto", "silva", "cabeça", "amarelo", "DST"];
+    i = Math.floor(Math.random() * 5)
+    vet[i]
+    botao.innerHTML= vet[i]
 }
